da01986a386fba32b5004bfdec9a643de086a1c5
Selorm <107414631+shaligah@users.noreply.github.com>
2023-02-02 20:10:15 +0000

    Update House Price Prediction.py

--- House Price Prediction.py
+++ House Price Prediction.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 #data manipulation
 import numpy as np
 import pandas as pd
@@ -25,76 +19,30 @@ from sklearn.model_selection import GridSearchCV
 #model evaulation
 from sklearn.metrics import r2_score
 
-
-# In[2]:
-
-
 data = pd.read_csv('train.csv')
-
-
-# In[3]:
-
-
 data.dtypes.value_counts()
-
-
-# In[4]:
-
-
 data.head()
-
-
-# In[5]:
-
-
 df_int = data.select_dtypes(include=['int64', 'float64'])
-
-
-# In[6]:
 
 
 #DEALING WITH MISING VALUES
 df_int.isna().mean()*100
 
-
-# In[7]:
-
-
 msno.matrix(df_int)
 
-
-# In[15]:
-
-
 df_int.isna().sum()
-
-
-# In[9]:
-
 
 #using imputation methods to fill in the missing data
 imp =SimpleImputer(strategy='mean')
 m_imp = df_int.copy()
 m_imp.loc[:,:] = imp.fit_transform(m_imp.loc[:,:])
 
-
-# In[10]:
-
-
 imp_2 = KNNImputer(n_neighbors = 2)
 knn_imp = df_int.copy()
 knn_imp.loc[:,:]=imp_2.fit_transform(knn_imp.loc[:,:])
 
-
-# In[11]:
-
-
 imputations = {'mean imputations': m_imp,
              'knn imputations': knn_imp}
-
-
-# In[12]:
-
 
 #visualizing how imputed data resembles original data
 fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize=(15,8))
@@ -107,57 +55,19 @@ for ax, key in zip(ax.flatten(), imputations):
 
 # The orange histograms represent the original data while the blue histograms represent the imputed data. From the graphs ot can be noticed that the missing data that was imputd using k-nearest neighbours followed the pattern of the original data unlike that using mean imputations.
 
-# In[13]:
-
-
 df_cat = data.select_dtypes(include='object')
 
-
-# In[14]:
-
-
 df_cat.isna().mean()*100
-
-
-# In[16]:
-
-
 df_cat.isna().sum()
-
-
-# In[17]:
-
 
 msno.matrix(df_cat)
 
-
-# In[18]:
-
-
 df_cat.drop(columns=['Alley', 'PoolQC', 'Fence','MiscFeature','FireplaceQu'], inplace=True)
-
-
-# In[19]:
-
-
 df_cat['Electrical'] = df_cat['Electrical'].fillna(df_cat['Electrical'].mode()[0])
-
-
-# In[20]:
-
-
 df_cat['MasVnrType'] = df_cat['MasVnrType'].fillna(df_cat['MasVnrType'].mode()[0])
-
-
-# In[21]:
-
 
 #using a unique value to represent the remaining missing values to maintain the originality of the data
 df_cat = df_cat.fillna('Unknown')
-
-
-# In[22]:
-
 
 #using a labelencoder to transform categorical variables into numerical variables for model
 copy = df_cat.copy()
@@ -167,77 +77,32 @@ for col in copy.columns:
 
 
 # # MODEL BUILDING WITHOUT FEATURE SELECTION
-
-# In[23]:
-
-
 X,y = knn_imp.loc[:,df_int.columns!='SalePrice'],knn_imp.loc[:,'SalePrice']
 
-
-# In[24]:
-
-
 new_d = pd.concat([X, copy], axis=1)
-
-
-# In[25]:
-
 
 #splitting the data into training and validation for model building and evaluation  
 X_train, X_test, y_train, y_test = train_test_split(new_d, y, test_size=0.3, random_state=42)
 
-
-# In[26]:
-
-
 model = xgt.XGBRegressor(random_state=42)
 model.fit(X_train, y_train)
 
-
-# In[27]:
-
-
 m = model.predict(X_test)
-
-
-# In[28]:
-
-
 r2_score(y_test,m)
-
-
-# In[29]:
 
 
 model2 = RandomForestRegressor()
 model2.fit(X_train, y_train)
 
-
-# In[30]:
-
-
 n = model2.predict(X_test)
-
-
-# In[31]:
-
-
 r2_score(y_test, n)
 
 
 # # MODEL BUILDING WITH FEATURE SELECTION
 
 # selecting features for numerical variables
-
-# In[32]:
-
-
 selector =  SelectKBest(f_regression,k='all')
 X_int =selector.fit_transform(X,y)
-
-
-# In[33]:
-
 
 names = X.columns.values[selector.get_support()]
 scores = selector.scores_[selector.get_support()]
@@ -245,10 +110,6 @@ names_scores = list(zip(names, scores))
 ns_df = pd.DataFrame(data = names_scores, columns=['Feat_names', 'F_Scores'])
 ns_df_sorted = ns_df.sort_values(['F_Scores', 'Feat_names'], ascending = [False, True])
 print(ns_df_sorted)
-
-
-# In[34]:
-
 
 #removing nunmerical variables for model buidling with F_score<10
 selector3 = SelectKBest(f_regression, k=20)
@@ -260,15 +121,8 @@ fea_int.shape
 
 # FEATURE SELECTION FOR CATEGORICAL VARIABLES
 
-# In[35]:
-
-
 selector2 =  SelectKBest(chi2,k='all')
 X_cat =selector2.fit_transform(copy,y)
-
-
-# In[36]:
-
 
 names2 = copy.columns.values[selector2.get_support()]
 scores2 = selector2.scores_[selector2.get_support()]
@@ -278,9 +132,6 @@ ns_df_sorted2 = ns_df2.sort_values(['F_Scores', 'Feat_names'], ascending = [Fals
 print(ns_df_sorted2)
 
 
-# In[37]:
-
-
 #removing variables with F-score <10
 selector4 =  SelectKBest(chi2,k=37)
 X_cat =selector4.fit_transform(copy,y)
@@ -288,93 +139,35 @@ names4 = copy.columns.values[selector4.get_support()]
 fea_cat = copy.loc[:,names4]
 fea_cat.shape
 
-
-# In[39]:
-
-
 m_data = pd.concat([fea_int, fea_cat], axis=1)
 
-
-# In[40]:
-
-
 X_train1, X_test1, y_train1, y_test1 = train_test_split(m_data, y, test_size=0.3, random_state=42)
-
-
-# In[41]:
 
 
 model3 = xgt.XGBRegressor(objective='reg:linear', random_state=42)
 model3.fit(X_train1, y_train1)
 
-
-# In[42]:
-
-
 pred3 = model3.predict(X_test1)
-
-
-# In[43]:
-
-
 r2_score(y_test1,pred3)
-
-
-# In[44]:
 
 
 model4 = RandomForestRegressor()
 model4.fit(X_train1, y_train1)
 
-
-# In[45]:
-
-
 pred4 = model4.predict(X_test1)
-
-
-# In[46]:
-
-
 r2_score(y_test1, pred4)
 
 
 # random forest regressor with selected features produced best outcome as such we would use it for prediction
 
 # # PREDICTIONS
-
-# In[47]:
-
-
 test = pd.read_csv('test.csv')
 
-
-# In[48]:
-
-
 test.dtypes.value_counts()
-
-
-# In[49]:
-
-
 test_int = test.select_dtypes(include=['int64', 'float64'])
-
-
-# In[50]:
-
-
 test_cat = test.select_dtypes(include='object')
 
-
-# In[70]:
-
-
 test_int.isnull().sum()
-
-
-# In[52]:
-
 
 test_int['BsmtFinSF1'] = test_int['BsmtFinSF1'].fillna(test_int['BsmtFinSF1'].mode()[0])
 test_int['BsmtFinSF2'] = test_int['BsmtFinSF2'].fillna(test_int['BsmtFinSF2'].mode()[0])
@@ -385,34 +178,12 @@ test_int['BsmtFullBath'] = test_int['BsmtFullBath'].fillna(test_int['BsmtFullBat
 test_int['GarageCars'] = test_int['GarageCars'].fillna(test_int['GarageCars'].mode()[0])
 test_int['GarageArea'] = test_int['GarageArea'].fillna(test_int['GarageArea'].mode()[0])
 
-
-# In[53]:
-
-
 test_int.loc[:,:] = imp_2.fit_transform(test_int.loc[:,:])
-
-
-# In[54]:
-
 
 test_int.drop(columns=['Id', 'MSSubClass','OverallCond','BsmtFinSF2','BsmtUnfSF','LowQualFinSF','BsmtFullBath', 'BsmtHalfBath','BedroomAbvGr', 'KitchenAbvGr','EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea','MiscVal', 'MoSold', 'YrSold'],inplace=True)
 
-
-# In[69]:
-
-
 test_cat.isnull().sum()
-
-
-# In[56]:
-
-
 test_cat.drop(columns=['Alley', 'PoolQC', 'Fence','MiscFeature','FireplaceQu'], inplace=True)
-
-
-# In[57]:
-
-
 test_cat['MSZoning'] = test_cat['MSZoning'].fillna(test_cat['MSZoning'].mode()[0])
 test_cat['Utilities'] = test_cat['Utilities'].fillna(test_cat['Utilities'].mode()[0])
 test_cat['Exterior1st'] = test_cat['Exterior1st'].fillna(test_cat['Exterior1st'].mode()[0])
@@ -421,76 +192,21 @@ test_cat['KitchenQual'] = test_cat['KitchenQual'].fillna(test_cat['KitchenQual']
 test_cat['Functional'] = test_cat['Functional'].fillna(test_cat['Functional'].mode()[0])
 test_cat['SaleType'] = test_cat['SaleType'].fillna(test_cat['SaleType'].mode()[0])
 
-
-# In[58]:
-
-
 test_cat = test_cat.fillna('Unknown')
-
-
-# In[59]:
-
 
 for col in test_cat.columns:
     test_cat[col] = enc.fit_transform(test_cat[col])
 
-
-# In[60]:
-
-
 test_cat.drop(columns='Street', inplace=True)
-
-
-# In[61]:
-
 
 test_data = pd.concat([test_int,test_cat], axis = 1)
 
-
-# In[62]:
-
-
 test_data.shape
-
-
-# In[63]:
-
 
 predicted = model4.predict(test_data)
 
-
-# In[64]:
-
-
 predicted = pd.DataFrame(predicted)
-
-
-# In[65]:
-
-
 predicted = predicted.rename({0:'SalePrice'},axis=1)
-
-
-# In[66]:
-
-
 predicted['Id'] = test['Id']
-
-
-# In[67]:
-
-
 predicted = predicted[['Id','SalePrice']]
-
-
-# In[68]:
-
-
 predicted
-
-
-# In[ ]:
-
-
-
-
